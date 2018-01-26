fdac08c8fe1140b58b4867ada3f08c2b9a27221a
zedinke <geleako@gmail.com>
2018-01-26 20:47:02 +0100

    Upload

---
+++ src/bindings/scripts/sql/create_database.sql
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+CREATE DATABASE `trinityscript` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON `trinityscript` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
+=======
+CREATE DATABASE `trinityscript` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON `trinityscript` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
+>>>>>>> [svn] * Continue Trinity Script conversion.
